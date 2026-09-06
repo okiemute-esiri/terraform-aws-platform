5206c09a534194e61e5797a75f3de076b178e836
Okiemute Esiri <okiemuteesiri214@gmail.com>
2026-09-06 13:32:52 +0100

    terraform: configure AWS provider

---
+++ providers.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region = var.aws_region
+
+  default_tags {
+    tags = local.common_tags
+  }
+}
+
+locals {
+  common_tags = {
+    Project     = var.project_name
+    Environment = var.environment
+    ManagedBy   = "Terraform"
+  }
+}
